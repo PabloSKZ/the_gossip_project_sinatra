e89de819c4e1f4c6f6b26c10058c3de9f590b141
PabloSKZ <pablo.eskenazi@gmail.com>
2020-02-03 17:13:28 +0100

    edit

---
+++ lib/gossip.rb
@@ -0,0 +1,30 @@
+class Gossip
+    attr_accessor :author, :content
+
+    def initialize(author, content)
+        @author = author
+        @content = content
+    end
+
+    def save
+        CSV.open("../db/gossip.csv", "ab") do |csv|
+          csv << [@author, @content]
+        end
+      end
+
+    def self.all
+        all_gossips = []
+        CSV.read("../db/gossip.csv").each do |csv_line|
+            all_gossips << Gossip.new(csv_line[0], csv_line[1])
+        end
+        return all_gossips
+    end
+
+    def self.find(id)
+        i = 0
+        CSV.read('../db/gossip.csv').each do |csv_line|
+            puts csv_line if i == id
+            i += 1
+        end
+    end
+end
